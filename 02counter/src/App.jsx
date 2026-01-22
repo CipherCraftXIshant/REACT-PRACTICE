bec6c036bf0b0c8b165bcaab723647274bdff9d3
Ishant Sharma <cipherxishant@Ishants-MacBook-Air.local>
2026-01-22 13:03:44 +0530

    Added 02counter React project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] =  useState(15)
+
+  // let counter = 5;
+
+  const addValue = () => {
+    // counter++;
+    if(counter<20){
+    setCounter(counter+1)
+    console.log("clicked" , counter);
+    }
+    else{
+      console.log("counter cant get above 20");
+    }
+  }
+
+  const removeValue = () => {
+    if(counter>0){
+      setCounter(counter-1);
+    }
+    else{
+      console.log("Counter cant go beyond 0")
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai or react</h1>
+      <h2>Counter value : {counter} </h2>
+
+      <button onClick={addValue} >Add Value</button>
+      <br />
+      <button onClick={removeValue} >Remove Value</button>
+    </>
+  )
+}
+
+export default App
